50608e4d17f8544fb8a7763ea13c2737edc056de
bayu <sektibayu@gmail.com>
2017-03-12 16:31:54 +0700

    soal3 sudah

--- tugas1/soal3/install.sh
+++ tugas1/soal3/install.sh
@@ -1,4 +1,23 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
 apt-get update
+
+#install apache dan php
 apt-get install -y apache2
-apt-get install -y php7.0
+apt-get install -y php7.0 libapache2-mod-php7.0
+
+#setting apache2
+VHOST=$(cat << EOF
+<VirtualHost *:80>
+	DocumentRoot "/var/www"
+	<Directory "/var/www">
+		AllowOverride All
+		Require all granted
+	</Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+
+sudo a2enmod rewrite
+
+service apache2 restart
